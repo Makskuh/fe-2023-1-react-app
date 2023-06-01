373fd989a1a69550b2ed581d89fceaffc9298377
Kyrylo <fresh.js.ftd.@gmail.com>
2023-06-01 18:45:15 +0300

    Add basic react element example

--- src/index.js
+++ src/index.js
@@ -5,11 +5,14 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+
+const elem = React.createElement(
+  'header', // рядок з назвою html елементу
+  { id: 'header' }, // об'єкт з атрибутами до цього елементу
+  'null' // 3+ параметрии - дочірні елементи, які будуть знаходитися всередині створюємого єлементу (не відображає null, undefined, false; кидає помилку якщо дають об'ект)
 );
+
+root.render(elem);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))

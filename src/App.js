5840f2dfdf800cf0f0b299e15722f80b079f4bb8
Kyrylo <fresh.js.ftd.@gmail.com>
2023-06-05 18:08:21 +0300

    Add homework task solutions

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
 import React from 'react';
 import Greeting from './components/Greeting';
 import Header from './components/Header';
+import Review from './components/Review';
+
+const data = {
+  id: 1,
+  title: 'Best device ever',
+  mark: 9.5,
+  text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+  authorName: 'User Userenko',
+  authorImg:
+    'https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress',
+  pros: ['cheap', 'efficient', 'feeds my cat'],
+  cons: ['bulky, cant place 2 of them in my house'],
+};
+
 class App extends React.Component {
   render() {
     return (
       <div>
-        <Header />
-        <Greeting name='User'  />
-        <Greeting name='Anton' />
+        {/* <Header data={data} /> */}
+        <Greeting name='User' />
+        <Review reviewData={data} />
         <Greeting />
       </div>
     );

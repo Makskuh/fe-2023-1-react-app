b00559f142666eecc676f0b659e0bf08c3f6d350
Kyrylo <fresh.js.ftd.@gmail.com>
2023-06-07 19:03:59 +0300

    Add controlled component example

--- src/components/LoginForm/index.jsx
+++ src/components/LoginForm/index.jsx
@@ -10,17 +10,43 @@ function loginUser(email, password) {
 }
 
 class LoginForm extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      email: '',
+      password: '',
+    };
+  }
+
   submitHandler = (e) => {
+    const { email, password } = this.state;
     e.preventDefault();
     console.log(e);
 
     // loginUser(e.target.elements.mail.value, e.target.elements.password.value);
+    loginUser(email, password);
+  };
+
+  handleEmailChange = (e) => {
+    this.setState({
+      email: e.target.value,
+    });
   };
 
   render() {
+    const { email } = this.state;
+
     return (
       <form onSubmit={this.submitHandler} className='form'>
-        <input className='input' type='email' name='mail' placeholder='Email' />
+        <input
+          onChange={this.handleEmailChange}
+          className='input'
+          type='email'
+          name='mail'
+          placeholder='Email'
+          value={email}
+        />
         <input
           className='input'
           type='password'

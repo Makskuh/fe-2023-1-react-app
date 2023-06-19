819f25606aa499bc95b3fa7a263ef72362c99b02
Kyrylo <fresh.js.ftd.@gmail.com>
2023-06-19 18:53:31 +0300

    Add bad mouse tracker solution

--- src/components/Counter/index.jsx
+++ src/components/Counter/index.jsx
@@ -1,21 +1,50 @@
 import React, { useState } from 'react';
 
 function Counter(props) {
-  const [state, setState] = useState(0);
+  const [state, setState] = useState({
+    clicks: 0,
+    x: 0,
+    y: 0,
+  });
 
-  const add = () => {
-    setState(state + 1);
+  const add = (e) => {
+    setState({
+      ...state,
+      clicks: state.clicks + 1,
+    });
   };
+
   const subtract = () => {
-    setState(state - 1);
+    setState({
+      ...state,
+      clicks: state.clicks - 1,
+    });
+  };
+
+  const mouseMoveHandler = (e) => {
+    const { clientX, clientY } = e;
+
+    setState({
+      ...state,
+      x: clientX,
+      y: clientY
+    });
   };
 
   return (
-    <>
-      <p>Counter is {state}</p>
+    <div
+      style={{
+        padding: '40px',
+        border: '5px solid black',
+      }}
+      onMouseMove={mouseMoveHandler}
+    >
+      <p>Counter is {state.clicks}</p>
+      <p>X coordinate is {state.x}</p>
+      <p>Y coordinate is {state.y}</p>
       <button onClick={add}>Add</button>
       <button onClick={subtract}>Subtract</button>
-    </>
+    </div>
   );
 }
 

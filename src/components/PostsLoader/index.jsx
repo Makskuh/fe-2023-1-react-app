b6bd351eeff4f82f2e08366609fc5ac7a9890b23
Kyrylo <fresh.js.ftd.@gmail.com>
2023-06-21 18:53:13 +0300

    Add useData custom hook

--- src/components/PostsLoader/index.jsx
+++ src/components/PostsLoader/index.jsx
@@ -1,29 +1,8 @@
-import { useEffect, useState } from 'react';
 import { getPosts } from '../../api';
+import { useData } from 'hooks';
 
 function PostsLoader(props) {
-  const [posts, setPosts] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
-
-  useEffect(() => {
-    load();
-  }, []);
-
-  const load = () => {
-    setIsLoading(true);
-
-    getPosts()
-      .then((posts) => {
-        setPosts(posts);
-      })
-      .catch((error) => {
-        setError(error);
-      })
-      .finally(() => {
-        setIsLoading(false);
-      });
-  };
+  const { data: posts, isLoading, error } = useData(getPosts);
 
   if (error) {
     return <div>{error.message}</div>;

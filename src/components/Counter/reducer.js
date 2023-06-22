356df9e2533ef0b68edcae32624614873eedb70f
Kyrylo <fresh.js.ftd.@gmail.com>
2023-06-22 20:45:25 +0300

    Refactor reducer to switch...case

--- src/components/Counter/reducer.js
+++ src/components/Counter/reducer.js
@@ -1,27 +1,33 @@
 export default function reducer(state, action) {
   const { type, payload } = action;
-  if (type === 'add') {
-    const newState = {
-      ...state,
-      counterValue: state.counterValue + state.step,
-    };
 
-    return newState;
-  } else if (type === 'subtract') {
-    const newState = {
-      ...state,
-      counterValue: state.counterValue - state.step,
-    };
+  switch (type) {
+    case 'add': {
+      const newState = {
+        ...state,
+        counterValue: state.counterValue + state.step,
+      };
 
-    return newState;
-  } else if (type === 'changeStep') {
-    const newState = {
-      ...state,
-      step: payload,
-    };
+      return newState;
+    }
+    case 'subtract': {
+      const newState = {
+        ...state,
+        counterValue: state.counterValue - state.step,
+      };
 
-    return newState;
+      return newState;
+    }
+    case 'changeStep': {
+      const newState = {
+        ...state,
+        step: payload,
+      };
+
+      return newState;
+    }
+
+    default:
+      return state;
   }
-
-  return state;
 }

6c95e7a6d2fa53562acedeeae0e8a04b32458ca2
Kyrylo <fresh.js.ftd.@gmail.com>
2023-06-23 20:16:55 +0300

    Add better Formik usage

--- src/components/forms/SignUpForm/index.jsx
+++ src/components/forms/SignUpForm/index.jsx
@@ -4,7 +4,7 @@ import { Formik, Form, Field, ErrorMessage } from 'formik';
 const initialState = {
   firstName: '',
   lastName: '',
-  email: '',
+  email: 'test@test.test',
   password: '',
   isAgree: false,
 };
@@ -13,53 +13,19 @@ function SignUpForm(props) {
   const onSumbit = (values, formikBag) => {
     console.log(values);
     console.log(formikBag);
+    formikBag.resetForm();
   };
 
   return (
     <Formik initialValues={initialState} onSubmit={onSumbit}>
-      {(formikProps) => {
-        console.log(formikProps);
-        return (
-          <form
-            onSubmit={formikProps.handleSubmit}
-            onReset={formikProps.handleReset}
-          >
-            <input
-              name='firstName'
-              placeholder='firstName'
-              value={formikProps.values.firstName}
-              onChange={formikProps.handleChange}
-            />
-            <input
-              name='lastName'
-              placeholder='lastName'
-              value={formikProps.values.lastName}
-              onChange={formikProps.handleChange}
-            />
-            <input
-              name='email'
-              type='email'
-              placeholder='email'
-              value={formikProps.values.email}
-              onChange={formikProps.handleChange}
-            />
-            <input
-              name='password'
-              type='password'
-              placeholder='password'
-              value={formikProps.values.password}
-              onChange={formikProps.handleChange}
-            />
-            <input
-              name='isAgree'
-              type='checkbox'
-              value={formikProps.values.isAgree}
-              onChange={formikProps.handleChange}
-            />
-            <button type='submit'>Submit</button>
-          </form>
-        );
-      }}
+      <Form>
+        <Field name='firstName' placeholder='firstName' />
+        <Field name='lastName' placeholder='lastName' />
+        <Field name='email' type='email' placeholder='email' />
+        <Field name='password' type='password' placeholder='password' />
+        <Field name='isAgree' type='checkbox' />
+        <button type='submit'>Submit</button>
+      </Form>
     </Formik>
   );
 }

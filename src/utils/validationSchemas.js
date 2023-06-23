709c7802f2a32eaaa590f39f91390541b6f82702
Kyrylo <fresh.js.ftd.@gmail.com>
2023-06-23 20:41:55 +0300

    Add custom validation text example

--- src/utils/validationSchemas.js
+++ src/utils/validationSchemas.js
@@ -6,10 +6,10 @@ export const NAME_SCHEMA = yup
   .required();
 
 export const SIGN_UP_SCHEMA = yup.object({
-  email: yup.string().email().required(),
+  email: yup.string().email().required('You must enter email'),
   password: yup
     .string()
-    .matches(/^(?=.{0,}[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[!#$%^&*]).{8,32}$/)
+    .matches(/^(?=.{0,}[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[!#$%^&*]).{8,32}$/, ' Password must be 8-32 symbols with lower and upper case letters, number and one of this symbols !#$%^&*')
     .required(),
   firstName: NAME_SCHEMA,
   lastName: NAME_SCHEMA,
